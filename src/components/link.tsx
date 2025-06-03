267637a808f648b76e43afd8aae79bf211a071ed
Võ Công Đạt <vo.cong.dat.fpt@gmail.com>
2025-06-03 22:30:00 +0700

    feat(home): update home page

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -3,5 +3,7 @@ import { cn } from '../lib/utils';
 import { buttonVariants } from './button';
 
 export default function ButtonLink({ variant, size, className, ...props }: any) {
-  return <Link {...props} className={cn(buttonVariants({ variant, size, className }))} />;
+  return (
+    <Link {...props} className={cn('text-white', buttonVariants({ variant, size, className }))} />
+  );
 }
--- src/pages/home/components/card.tsx
+++ src/pages/home/components/card.tsx
@@ -1,12 +1,12 @@
 export default function Card() {
   return (
-    <div className="w-[calc(25%-15px)]">
+    <div className="md:w-[calc(25%-15px)] w-[287px] shrink-0">
       <img
         src="https://placehold.co/600x400"
         alt=""
         className="rounded-[8px] max-h-[206px] object-cover w-full"
       />
-      <div className="mx-3 py-5 px-4 bg-white relative -top-9 rounded-[8px]">
+      <div className="mx-3 py-5 px-4 bg-white relative -top-9 shadow rounded-[8px]">
         <div className="font-bold text-lg text-[#1D2939]">PTE Achievers 30-36-42</div>
         <div className="text-[#667085] font-medium">
           This comprehensive program covers all aspects of the test, including speaking, writing,
