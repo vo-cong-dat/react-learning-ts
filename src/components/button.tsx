d4bd08dd500bd9dd2bf6d52cb23636b4848f703b
Võ Công Đạt <vo.cong.dat.fpt@gmail.com>
2025-06-26 20:44:45 +0700

    feat(button): update style button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,37 +1,33 @@
-import * as React from 'react';
-import { Slot } from '@radix-ui/react-slot';
-import { cva, type VariantProps } from 'class-variance-authority';
-import { cn } from '@/utils/cn';
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/utils/cn";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-lg leading-[22px] font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center cursor-pointer rounded-lg justify-center gap-3 whitespace-nowrap text-lg leading-[100%] font-medium transition-all disabled:pointer-events-none [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none",
   {
     variants: {
       variant: {
-        primary: '',
-        secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-        link: 'text-primary underline-offset-4 hover:underline',
-        elevated: '',
-
-        default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-        outline:
-          'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+        primary:
+          "bg-primary-100 hover:shadow-button text-white hover:bg-primary-200 focus-visible:bg-primary-200 disabled:bg-grey-100 disabled:text-grey-200",
+        secondary:
+          "border-secondary-100 text-primary-100 border bg-white hover:bg-primary-100/3 focus-visible:bg-primary-100/3 disabled:bg-grey-100 disabled:text-grey-200 disabled:border-0",
+        ghost:
+          "bg-transparent text-primary-100 hover:shadow-button hover:bg-primary-200/3 focus-visible:bg-primary-200/3 disabled:bg-transparent disabled:text-grey-200",
+        link: "text-primary underline-offset-4 hover:underline",
+        elevated:
+          "bg-white shadow-button text-primary-100 hover:bg-primary-200/3 focus-visible:bg-primary-200/3 disabled:bg-grey-100 disabled:text-grey-200",
       },
       size: {
-        default: 'px-5 py-4 has-[>svg]:px-3',
-        sm: 'h-8 rounded-md gap-1 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-9',
+        default: "px-5 py-4 h-[54px]",
+        icon: "h-11 p-3",
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      variant: "primary",
+      size: "default",
     },
-  }
+  },
 );
 
 function Button({
@@ -39,19 +35,28 @@ function Button({
   variant,
   size,
   asChild = false,
+  children,
+  startIcon,
+  endIcon,
   ...props
-}: React.ComponentProps<'button'> &
+}: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
+    startIcon?: React.ReactNode;
+    endIcon?: React.ReactNode;
   }) {
-  const Comp = asChild ? Slot : 'button';
+  const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
-    />
+    >
+      {startIcon}
+      {children}
+      {endIcon}
+    </Comp>
   );
 }
 
---
+++ src/components/icons.tsx
@@ -0,0 +1,2 @@
+export { default as ShoppingCartIcon } from "../assets/icons/shopping-cart-icon.svg?react";
+export { default as ArrowRightIcon } from "../assets/icons/arrow-right-icon.svg?react";
